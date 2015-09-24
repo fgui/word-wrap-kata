46f74651ddb251f0a03af54b28c1ac060d9a92f2
francesc <francesc.guillen@gmail.com>
2015-09-24 18:04:30 +0200

    test multiline that doesn't require split

--- src/word_wrap/core.clj
+++ src/word_wrap/core.clj
@@ -1,10 +1,17 @@
 (ns word-wrap.core)
 
+(defn lines
+  "Splits into lines which are shorter than column.
+  Should respect original split of lines.
+  "
+  [^String text ^long column]
+  (clojure.string/split text #"\n"))
+
 (defn wrap
   "The function returns the string, but with line breaks inserted at just
   the right places to make sure that no line is longer than the column number.
   Ttry to break lines at word boundaries.
   Like a word processor, break the line by replacing the last space ine
   a line with a newline."
-  [^String text ^long columns]
-  text)
+  [^String text ^long column]
+  (clojure.string/join \newline (lines text)))
--- test/word_wrap/core_test.clj
+++ test/word_wrap/core_test.clj
@@ -1,6 +1,11 @@
-(ns word-wrap.core-test
+ (ns word-wrap.core-test
   (:require [clojure.test :refer :all]
             [word-wrap.core :refer :all]))
 
-(deftest a-wrapper
-  (testing "simple test on line" (is (= "hola" (wrap "hola" 5)))))
+(deftest wrap-test
+  (testing "simple test on line"
+    (is (= "hola" (wrap "hola" 5)))
+    (is (= "hola" (wrap "hola" 3))))
+  (testing "multi-line ok"
+    (is (= "hola\nadeu" (wrap "hola\nadeu" 4))))
+  )
