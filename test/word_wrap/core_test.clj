3c6ea50ef180b5d6c0b88d118125ad386235b00d
francesc <francesc.guillen@gmail.com>
2015-09-25 13:05:54 +0200

    test with space anywhere any multiple breaks.

--- test/word_wrap/core_test.clj
+++ test/word_wrap/core_test.clj
@@ -9,4 +9,8 @@
   (testing "multi-line ok" (is (= "hola\nadeu" (wrap "hola\nadeu" 4))))
   (testing "split no space" (is (= "hola\nadeu" (wrap "holaadeu" 4))))
   (testing "split w space" (is (= "hola\nadeu" (wrap "hola adeu" 4))))
-  )
+  (testing "split on a space" (is (= "hola\nadeu" (wrap "hola adeu" 6))))
+  (testing "split 3 times" (is (= "hola\nadeu\nbyebye"
+                                  (wrap "hola adeu byebye" 6))))
+  (testing "keep a space" (is (= "ho la\nadeu\nbyebye"
+                                 (wrap "ho la adeu byebye" 6)))))
